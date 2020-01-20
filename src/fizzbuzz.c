4fc533cf3fe39bd23d98fc597ab53f6025947d5a
lillianna.h.blair <lillianna.h.blair@accenture.com>
2020-01-20 09:56:39 -0500

    fizzbuzz complete

--- src/fizzbuzz.c
+++ src/fizzbuzz.c
@@ -1,6 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "fizzbuzz.h"
 
 const char *fizzbuzz(int number) {
-    return "1";
+    if (number % 3 == 0 && number % 5 == 0) {
+        return "FizzBuzz";
+    } else if (number % 3 == 0) {
+        return "Fizz";
+    } else if (number % 5 == 0) {
+        return "Buzz";
+    } else {
+        static char result[5];
+        snprintf(result, 5, "%d",number);
+        return result;
+    }
 }
--- test/test_fizzbuzz.c
+++ test/test_fizzbuzz.c
@@ -11,6 +11,27 @@ TEST(FizzBuzz, returns1_whenPassed1) {
     TEST_ASSERT_EQUAL_STRING("1", fizzbuzz(1));
 }
 
+TEST(FizzBuzz, returnsFizz_whenPassed3) {
+    TEST_ASSERT_EQUAL_STRING("Fizz", fizzbuzz(3));
+}
+
+TEST(FizzBuzz, returnsBuzz_whenPassed5) {
+    TEST_ASSERT_EQUAL_STRING("Buzz", fizzbuzz(5));
+}
+
+TEST(FizzBuzz, returnsFizzBuzz_whenPassed15) {
+    TEST_ASSERT_EQUAL_STRING("FizzBuzz", fizzbuzz(15));
+}
+
+TEST(FizzBuzz, returnsFizzBuzz_whenPassed30) {
+    TEST_ASSERT_EQUAL_STRING("FizzBuzz", fizzbuzz(30));
+}
+
+
 TEST_GROUP_RUNNER(FizzBuzz) {
     RUN_TEST_CASE(FizzBuzz, returns1_whenPassed1);
+    RUN_TEST_CASE(FizzBuzz, returnsFizz_whenPassed3);
+    RUN_TEST_CASE(FizzBuzz, returnsBuzz_whenPassed5);
+    RUN_TEST_CASE(FizzBuzz, returnsFizzBuzz_whenPassed15);
+    RUN_TEST_CASE(FizzBuzz, returnsFizzBuzz_whenPassed30);
 }
